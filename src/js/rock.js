3b9f5d187e37c124257ab15172db7bb173f5266f
Alina-Makarenko <alinamakarenko1807@gmail.com>
2026-02-04 19:29:38 +0200

    rock2

--- src/js/rock.js
+++ src/js/rock.js
@@ -1,54 +1,60 @@
 let wins = 0;
 let losses = 0;
 let draws = 0;
-const rockBtnEl = document.querySelectorAll("li.rock__item > button");
-const rockTextEl = document.getElementById("message");
-const rockTextRef = document.getElementById("score");
+const rockBtnEl = document.querySelectorAll('li.rock__item > button');
+const rockTextEl = document.getElementById('message');
+const rockMessageRef = document.getElementById('computerChoise');
+const rockTextRef = document.getElementById('score');
+const rockBtnRef = document.querySelector('.rock__button');
 
 rockBtnEl.forEach((item, i) => {
-  item.addEventListener("click", (event) => {
-    const userChoise = i;
+  item.addEventListener('click', event => {
+      const userChoise = i;
     const computerChoise = Math.floor(Math.random() * 3);
     chooseWinner(userChoise, computerChoise);
   });
 });
 
-const updateScore =(losses, wins,draws) => {
-    rockTextRef.textContent = 
-`Рахунок:
+const updateScore = (losses, wins, draws) => {
+  rockTextRef.textContent = `Рахунок:
 Комп’ютер - ${losses}
 Ви - ${wins}
 Нічия - ${draws}`;
-}
+};
 
 const chooseWinner = (userChoise, computerChoise) => {
-  const draw = "Нічия";
-  const yourWin = "Ви виграли раунд!";
-  const yourLosse = "Комп’ютер виграв раунд!";
+  const draw = 'Нічия';
+  const yourWin = 'Ви виграли раунд!';
+  const yourLosse = 'Комп’ютер виграв раунд!';
 
+  rockTextEl.classList.remove('colorwin', 'colorlosse', "color__draws");
   if (userChoise === computerChoise) {
     rockTextEl.textContent = draw;
     draws = draws + 1;
-  } else if (userChoise === 0 && computerChoise === 1) {
+    rockTextEl.classList.add('color__draws');
+  } else if (
+    (userChoise === 0 && computerChoise === 1) ||
+    (userChoise === 1 && computerChoise === 2) ||
+    (userChoise === 2 && computerChoise === 0)
+  ) {
     rockTextEl.textContent = yourWin;
-    wins = wins + 1
-  } else if (userChoise === 0 && computerChoise === 2) {
+    wins = wins + 1;
+    rockTextEl.classList.add('color__win');
+  } else if (
+    (userChoise === 0 && computerChoise === 2) ||
+    (userChoise === 1 && computerChoise === 0) ||
+    (userChoise === 2 && computerChoise === 1)
+  ) {
     rockTextEl.textContent = yourLosse;
-    losses = losses +1
-  } else if (userChoise === 1 && computerChoise === 0) {
-    rockTextEl.textContent = yourLosse;
-    losses = losses +1
-  } else if (userChoise === 1 && computerChoise === 2) {
-    rockTextEl.textContent = yourWin;
-    wins = wins + 1
-  } else if (userChoise === 2 && computerChoise === 0) {
-    rockTextEl.textContent = yourWin;
-    wins = wins + 1
-  } else if (userChoise === 2 && computerChoise === 1) {
-    rockTextEl.textContent = yourLosse;
-    losses = losses +1
+    losses = losses + 1;
+    rockTextEl.classList.add('color__losse');
   }
-  updateScore (losses, wins,draws )
-  
-  return rockTextEl.textContent
+  updateScore(losses, wins, draws);
+
+  return rockTextEl.textContent;
 };
+// const computerLog = (computerChoise) => {
+//   rockBtnRef.addEventListener ("click", (event) => {
+//     rockMessageRef.value = Варіант комп’ютера:${computerChoise}
+//   });
+// }
