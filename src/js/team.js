278564b45fdfab39b10894605dec71d46b30e194
SviatoslavFedosiuk <fedosiuksv11@icloud.com>
2026-02-05 21:32:32 +0200

    team-second

---
+++ src/js/team.js
@@ -0,0 +1,87 @@
+const team = [
+  {
+    img: '/img/oleksii.webp',
+    name: 'Олексій',
+    done: 'Team lead, секції: calculator і dinosaur',
+  },
+  {
+    img: '/img/sviatoslav.webp',
+    name: 'Святослав',
+    done: 'Scrum-master, секції: yearNumber, team і footer',
+  },
+  {
+    img: '/img/alina.webp',
+    name: 'Аліна',
+    done: 'Розробляла секції: rock, football і scientists',
+  },
+
+  {
+    img: '/img/lev.webp',
+    name: 'Лев',
+    done: 'Розробляв секції: header, footermodal і modal',
+  },
+];
+
+const teamListRef = document.querySelector('.team__list');
+
+function getArray({ img, name, done }) {
+  return `<li class="team__item">
+            <img src="${img}" alt="${name}" class="team__img">
+            <h2 class="team__text">${name}</h2>
+            <h3 class="team__description">${done}</h3>
+
+          </li>`;
+}
+
+const join = team.map(getArray).join('');
+teamListRef.innerHTML = join;
+
+const teamItemRef = document.querySelectorAll(".team__item");
+
+const teamBoxRef = document.querySelectorAll(".team__box");
+
+const leftBtnRef = document.querySelector(".team__button-left");
+const rightBtnRef = document.querySelector(".team__button-right");
+
+let index = 0;
+
+window.addEventListener('keydown', event => {
+  if (event.key === 'ArrowRight') {
+    if (index < teamItemRef.length - 1) {
+      index += 1;
+      moveSlider();
+    }
+  }
+
+  if (event.key === 'ArrowLeft') {
+    if (index > 0) {
+      index -= 1;
+
+      moveSlider();
+    }
+  }
+});
+function moveSlider() {
+  const update = -index * 100;
+  teamListRef.style.transform = `translateX(${update}%)`;
+    console.log(update);
+
+    teamBoxRef.forEach(box => box.classList.remove("team__active"));
+  teamBoxRef[index].classList.add("team__active");
+}
+
+rightBtnRef.addEventListener('click', () => {
+  if (index < teamItemRef.length - 1) {
+    index += 1;
+    moveSlider();
+  }
+});
+leftBtnRef.addEventListener('click', () => {
+  if (index > 0) {
+    index -= 1;
+
+    moveSlider();
+  }
+});
+
+
